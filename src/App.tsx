a90dbc3f2d336bf485d407116d74313c1cde5fde
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-03-15 11:31:04 -0300

    setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* https://www.npmjs.com/package/react-simple-typewriter for next feature*/}
+                <p className='App-header-title'>tab_</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
