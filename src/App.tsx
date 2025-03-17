f2d4c8d9e6e3d88238d24bc317ff7ffa34530134
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-03-16 21:13:37 -0300

    Title implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Typewriter } from 'react-simple-typewriter'
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 {/* https://www.npmjs.com/package/react-simple-typewriter for next feature*/}
-                <p className='App-header-title'>tab_</p>
+                <div className='App-header-div-slogan'>
+                    <div className='App-header-title'>
+                        <Typewriter typeSpeed={200} words={['tab']} cursorStyle="_" cursor />
+                    </div>
+                    <div className='App-header-slogan-text'>
+                        <Typewriter typeSpeed={200} words={['Tu solucion', 'somos nosotros']} />
+                    </div>
+                </div>
             </header>
         </div>
     );
