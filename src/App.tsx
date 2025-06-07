7fddff964e19c717885035ef00fbaee0154174b6
Manuel Tomas Teves <manuel.tomas.teves@gmail.com>
2025-06-07 17:53:24 -0300

    Some features of about us section

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import './App.css';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
+import AboutUs from './components/AboutUs/AboutUs';
 
 function App() {
     return (
         <div className="App">
             <Hero />
             <Navbar />
-
-            <div className='App-us-section'>
-                
-            </div>
+            <AboutUs />
         </div>
     );
 }
