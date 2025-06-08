5e7c7a2f0cab686aff3588c4bcbc007f222970b0
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-06-08 13:38:55 -0300

    Fix responsive

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -5,10 +5,10 @@ function AboutUs() {
     return (
         <div className='App-us-section'>
             <div className='App-us-section-illustrationContainer'>
-                <img src={require("./logo.gif")} alt="Logo of tab_"></img>
+                <img src={require("./logo.gif")} alt="Logo of tab_" style={{width: '100%'}}></img>
             </div>
             <div className='App-us-section-descContainer'>
-                <h1 className='App-us-section-title'>NOSOTROS</h1>
+                <h1 className='subTitle'>NOSOTROS</h1>
                 <p className='App-us-section-desc'>
                     La solución tecnológica que tu negocio necesita. Diseñamos páginas web impactantes, potenciamos tu marca con diseño creativo y optimizamos tu rendimiento con el mejor hardware a medida. Nos enfocamos en maximizar tus ventas con soluciones reales, concretas y personalizadas.
                 </p>
@@ -18,10 +18,12 @@ function AboutUs() {
                 <p  className='App-us-section-desc noMargin'>
                     ¿Estás listo para dar el salto?
                 </p>
-                <div className='App-us-section-contactUsButtonContainer'>
-                    <div className='App-us-section-contactUsButton'>
-                        <p className='noMargin'>👉 Contactanos hoy y </p>
-                        <p className='noMargin'>transformá tu negocio.</p>
+                <div className='App-us-section-contactUsButtonMainContainer'>
+                    <div className='App-us-section-contactUsButtonContainer'>
+                        <div className='App-us-section-contactUsButton'>
+                            <p className='noMargin'>👉 Contactanos hoy y </p>
+                            <p className='noMargin'>transformá tu negocio.</p>
+                        </div>
                     </div>
                 </div>
             </div>
