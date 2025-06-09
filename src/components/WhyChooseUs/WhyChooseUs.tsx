3c265bf131e5240f8a2b5f61d74a8cecee51e554
Manuel Tomas Teves <manuel.tomas.teves@gmail.com>
2025-06-09 14:39:46 -0300

    Add responsive

--- src/components/WhyChooseUs/WhyChooseUs.tsx
+++ src/components/WhyChooseUs/WhyChooseUs.tsx
@@ -4,7 +4,7 @@ import './WhyChooseUs.css'
 function WhyChooseUs() {
     return (
         <div className="whyChooseUs-mainContainer">
-            <h1 className='subTitle' style={{marginBottom: 0}}>¿POR QUE ELEGIRNOS?</h1>
+            <h1 className='subTitle whyChooseUs-title'>¿POR QUE ELEGIRNOS?</h1>
             <p className="WhyChooseUs-desc text-desc">Entendemos que tu tiempo y tu inversión valen. No vendemos humo: ofrecemos soluciones tecnológicas concretas que funcionan. Diseñamos páginas web que convierten, marcas que se recuerdan y hardware que rinde al máximo.</p>
         </div>
     );
