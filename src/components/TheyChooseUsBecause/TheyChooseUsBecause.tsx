920a9e8d0a1acf03251f45ec2805c80627b33c48
Manuel Tomas Teves <manuel.tomas.teves@gmail.com>
2025-06-13 09:46:32 -0300

    Terminal implementation

--- src/components/TheyChooseUsBecause/TheyChooseUsBecause.tsx
+++ src/components/TheyChooseUsBecause/TheyChooseUsBecause.tsx
@@ -17,21 +17,27 @@ function TheyChooseUsBecause() {
                             <h1 className="TheyChooseUsBecause-descText noMargin">[ i ]</h1>
                             <h1 className="TheyChooseUsBecause-descTextB noMargin">Hacemos facil lo complejo__ </h1>
                         </div>
-                        <h1 className="noMargin">✅</h1>
+                        <div className="TheyChooseUsBecause-checkContainer">
+                            <h1 className="noMargin">✅</h1>
+                        </div>
                     </div>
                     <div className="TheyChooseUsBecause-rowText TheyChooseUsBecause-normalRow">
                         <div className="TheyChooseUsBecause-textContainer">
                             <h1 className="TheyChooseUsBecause-descText noMargin">[ i ]</h1>
                             <h1 className="TheyChooseUsBecause-descTextB noMargin">Nos adaptamos a tu presupuesto__ </h1>
                         </div>
-                        <h1 className="noMargin">✅</h1>
+                        <div className="TheyChooseUsBecause-checkContainer">
+                            <h1 className="noMargin">✅</h1>
+                        </div>
                     </div>
                     <div className="TheyChooseUsBecause-rowText TheyChooseUsBecause-normalRow">
                         <div className="TheyChooseUsBecause-textContainer">
                             <h1 className="TheyChooseUsBecause-descText noMargin">[ i ]</h1>
                             <h1 className="TheyChooseUsBecause-descTextB noMargin">Garantizamos calidad__ </h1>
                         </div>
-                        <h1 className="noMargin">✅</h1>
+                        <div className="TheyChooseUsBecause-checkContainer">
+                            <h1 className="noMargin">✅</h1>
+                        </div>
                     </div>
                 </div>
             </div>
